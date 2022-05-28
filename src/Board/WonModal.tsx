95151ff07892edc842d366bdfe8b0a10e8aeaa4a
Ammar Githam <ammargitham786@gmail.com>
2022-05-28 17:18:15 +0900

    Convert WonModal.js to tsx

---
+++ src/Board/WonModal.tsx
@@ -0,0 +1,41 @@
+import { Modal } from "../Modal";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & .header {
+    color: white;
+    font-size: 1.8rem;
+    font-weight: bold;
+    text-shadow: 0px 1px 0px #000000;
+  }
+
+  & .actions {
+    display: flex;
+    flex-direction: row;
+    margin-top: 1rem;
+    justify-content: center;
+  }
+`;
+
+type WonModalProps = {
+  open?: boolean,
+  onNewGameClick?: () => void,
+}
+
+export const WonModal = ({ open, onNewGameClick }: WonModalProps) => {
+  return (
+    <Modal open={open}>
+      <Container>
+        <span className="header">Game won!</span>
+        <div className="actions">
+          <button className="btn actionBtn" onClick={onNewGameClick}>
+            New Game
+          </button>
+        </div>
+      </Container>
+    </Modal>
+  );
+};
